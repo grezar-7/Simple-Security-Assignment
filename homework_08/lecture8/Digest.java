632b22ef4bbb547c949e99a22eb7a0be853dba4c
Daniel Jandrin <grezar1993@gmail.com>
2021-11-02 22:14:01 -0500

    assignment finished

--- homework_08/lecture8/Digest.java
+++ homework_08/lecture8/Digest.java
@@ -9,7 +9,15 @@ public class Digest {
 	 * print out the hash digest of the string 
 	 * "test message to be hashed" using the SHA-256 algorithm
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchAlgorithmException {
 		// TODO
+		String testMessage = "test message to be hashed";
+		MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		byte[] messageHashedArray = digest.digest(testMessage.getBytes());
+
+	 String hashedMessage = Base64.getEncoder().encodeToString(messageHashedArray);
+
+	 System.out.println("Original message is: " + testMessage);
+	 System.out.println("Hashed Message is: " + hashedMessage);
 	}
 }
